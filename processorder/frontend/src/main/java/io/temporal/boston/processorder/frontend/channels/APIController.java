02dbe51d32d3298de215a473e0dace70ec73fb36
Mike Nichols <mike.nichols@temporal.io>
2024-01-23 10:34:35 -0500

    fixed some params for clarity

--- processorder/frontend/src/main/java/io/temporal/boston/processorder/frontend/channels/APIController.java
+++ processorder/frontend/src/main/java/io/temporal/boston/processorder/frontend/channels/APIController.java
@@ -14,19 +14,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @Controller
 public class APIController {
     @Autowired
     WorkflowClient temporalClient;
 
-    @PostMapping(
-            value = "/orders",
-            consumes = {MediaType.APPLICATION_JSON_VALUE},
-            produces = {MediaType.TEXT_HTML_VALUE})
-    ResponseEntity post(@RequestBody PostOrdersParams params) {
-
+    private Map<String, ProcessOrderRequest> toProcessOrderRequests(PostOrdersParams params) {
+        HashMap<String, ProcessOrderRequest> requests = new HashMap<String, ProcessOrderRequest>();
         for(int i = 0; i < params.getCount(); i++) {
             String orderId = String.format("%s-%s", "order",CUID.randomCUID1());
+            String productId = params.getProductId();
+            if(Objects.equals(productId, "")) {
+                productId = String.format("%s-%s","product", CUID.randomCUID1());
+            }
             WorkflowOptions opts = WorkflowOptions.newBuilder().
                     setWorkflowId(orderId).
                     setTaskQueue("orders").
@@ -37,11 +41,28 @@ public class APIController {
             payment.setAccountId(CUID.randomCUID1().toString());
             // this would come from payment processor
             payment.setAuthorizedFundsToken(CUID.randomCUID1().toString());
-            request.setProductId(CUID.randomCUID1().toString());
+            request.setProductId(productId);
             request.setCustomerId(CUID.randomCUID1().toString());
             request.setOrderId(orderId);
             request.setPaymentInfo(payment);
-            WorkflowClient.start(workflowStub::execute, request);
+            requests.put(orderId, request);
+        }
+        return requests;
+    }
+    @PostMapping(
+            value = "/orders",
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.TEXT_HTML_VALUE})
+    ResponseEntity post(@RequestBody PostOrdersParams params) {
+
+        Map<String, ProcessOrderRequest> requests = toProcessOrderRequests(params);
+        for(Map.Entry<String, ProcessOrderRequest> entry: requests.entrySet()) {
+            WorkflowOptions opts = WorkflowOptions.newBuilder().
+                    setWorkflowId(entry.getKey()).
+                    setTaskQueue("orders").
+                    build();
+            ProcessOrder workflowStub = temporalClient.newWorkflowStub(ProcessOrder.class, opts);
+            WorkflowClient.start(workflowStub::execute, entry.getValue());
         }
 
         HttpStatus status = HttpStatus.OK ;
--- processorder/frontend/src/main/java/io/temporal/boston/processorder/frontend/channels/PostOrdersParams.java
+++ processorder/frontend/src/main/java/io/temporal/boston/processorder/frontend/channels/PostOrdersParams.java
@@ -2,6 +2,7 @@ package io.temporal.boston.processorder.frontend.channels;
 
 public class PostOrdersParams {
     private int count;
+    private String productId;
 
     public PostOrdersParams() {
     }
@@ -12,5 +13,13 @@ public class PostOrdersParams {
 
     public void setCount(int count) {
         this.count = count;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
     }
 }
